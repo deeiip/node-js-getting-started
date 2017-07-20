79add83317da90d8d3de5c375d162b863ae23457
Dipanjan <dipanjan.nag@tcs.com>
2017-07-20 21:09:53 +0530

    simplified chatbot

--- index.js
+++ index.js
@@ -5,16 +5,40 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.get('/message/:text', function(request, response) {
+  // response.render('pages/index');
+  var http = require("https");
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+  var options = {
+    "method": "POST",
+    "hostname": "hqn6l7zcjk.execute-api.us-east-1.amazonaws.com",
+    "port": null,
+    "path": "/dev/message",
+    "headers": {
+      "content-type": "application/json",
+      "cache-control": "no-cache"
+    }
+  };
+
+  var req = http.request(options, function (res) {
+    var chunks = [];
+
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on("end", function () {
+      var body = Buffer.concat(chunks);
+      // console.log(body.toString());
+      response.setHeader('Content-Type', 'application/json');
+      response.send(body);
+    });
+  });
+  console.log(request.params.text)
+  req.write(JSON.stringify({ message: request.params.text }));
+  req.end();
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
